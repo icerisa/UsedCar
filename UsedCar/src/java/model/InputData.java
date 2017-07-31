c2f0516674064edcfc4a3c44729546280ce23d01
yacth_Mon <yacthmon@gmail.com>
2017-08-01 03:57:55 +0700

    Add getter setter

--- UsedCar/src/java/model/InputData.java
+++ UsedCar/src/java/model/InputData.java
@@ -10,25 +10,25 @@ package model;
  * @author yacth_Mon
  */
 public class InputData {
-    int gradeTent;
-    int brand;
+    int gradeTent; //
+    int brandId; //
     String model;
     int year;
     int month;
-    int subModel;
+    int subModel;//
     long middlePrice;
-    int NCB;
+    int NCB;//
     String occupation;
     int income;
     int dept;
-    int guarantee;
+    int guarantee;//
 
     public InputData() {
     }
 
     public InputData(int gradeTent, int brand, String model, int year, int month, int subModel, long middlePrice, int NCB, String occupation, int income, int dept, int guarantee) {
         this.gradeTent = gradeTent;
-        this.brand = brand;
+        this.brandId = brand;
         this.model = model;
         this.year = year;
         this.month = month;
@@ -38,6 +38,102 @@ public class InputData {
         this.occupation = occupation;
         this.income = income;
         this.dept = dept;
+        this.guarantee = guarantee;
+    }
+
+    public int getGradeTent() {
+        return gradeTent;
+    }
+
+    public void setGradeTent(int gradeTent) {
+        this.gradeTent = gradeTent;
+    }
+
+    public int getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(int brandId) {
+        this.brandId = brandId;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getSubModel() {
+        return subModel;
+    }
+
+    public void setSubModel(int subModel) {
+        this.subModel = subModel;
+    }
+
+    public long getMiddlePrice() {
+        return middlePrice;
+    }
+
+    public void setMiddlePrice(long middlePrice) {
+        this.middlePrice = middlePrice;
+    }
+
+    public int getNCB() {
+        return NCB;
+    }
+
+    public void setNCB(int NCB) {
+        this.NCB = NCB;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public int getIncome() {
+        return income;
+    }
+
+    public void setIncome(int income) {
+        this.income = income;
+    }
+
+    public int getDept() {
+        return dept;
+    }
+
+    public void setDept(int dept) {
+        this.dept = dept;
+    }
+
+    public int getGuarantee() {
+        return guarantee;
+    }
+
+    public void setGuarantee(int guarantee) {
         this.guarantee = guarantee;
     }
     
