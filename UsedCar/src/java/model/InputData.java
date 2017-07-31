9b3555845d1ae053cf9e653833d77f5073e09074
yacth_Mon <yacthmon@gmail.com>
2017-08-01 03:50:56 +0700

    Redesign datatype

--- UsedCar/src/java/model/InputData.java
+++ UsedCar/src/java/model/InputData.java
@@ -10,23 +10,23 @@ package model;
  * @author yacth_Mon
  */
 public class InputData {
-    String gradeTent;
-    String brand;
+    int gradeTent;
+    int brand;
     String model;
-    String year;
-    String month;
-    String subModel;
-    String middlePrice;
-    String NCB;
+    int year;
+    int month;
+    int subModel;
+    long middlePrice;
+    int NCB;
     String occupation;
-    String income;
-    String dept;
-    String guarantee;
+    int income;
+    int dept;
+    int guarantee;
 
     public InputData() {
     }
 
-    public InputData(String gradeTent, String brand, String model, String year, String month, String subModel, String middlePrice, String NCB, String occupation, String income, String dept, String guarantee) {
+    public InputData(int gradeTent, int brand, String model, int year, int month, int subModel, long middlePrice, int NCB, String occupation, int income, int dept, int guarantee) {
         this.gradeTent = gradeTent;
         this.brand = brand;
         this.model = model;
@@ -38,103 +38,8 @@ public class InputData {
         this.occupation = occupation;
         this.income = income;
         this.dept = dept;
-        this.guarantee = guarantee;
-    }
-
-    public String getGradeTent() {
-        return gradeTent;
-    }
-
-    public void setGradeTent(String gradeTent) {
-        this.gradeTent = gradeTent;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public String getMonth() {
-        return month;
-    }
-
-    public void setMonth(String month) {
-        this.month = month;
-    }
-
-    public String getSubModel() {
-        return subModel;
-    }
-
-    public void setSubModel(String subModel) {
-        this.subModel = subModel;
-    }
-
-    public String getMiddlePrice() {
-        return middlePrice;
-    }
-
-    public void setMiddlePrice(String middlePrice) {
-        this.middlePrice = middlePrice;
-    }
-
-    public String getNCB() {
-        return NCB;
-    }
-
-    public void setNCB(String NCB) {
-        this.NCB = NCB;
-    }
-
-    public String getOccupation() {
-        return occupation;
-    }
-
-    public void setOccupation(String occupation) {
-        this.occupation = occupation;
-    }
-
-    public String getIncome() {
-        return income;
-    }
-
-    public void setIncome(String income) {
-        this.income = income;
-    }
-
-    public String getDept() {
-        return dept;
-    }
-
-    public void setDept(String dept) {
-        this.dept = dept;
-    }
-
-    public String getGuarantee() {
-        return guarantee;
-    }
-
-    public void setGuarantee(String guarantee) {
         this.guarantee = guarantee;
     }
     
+
 }
